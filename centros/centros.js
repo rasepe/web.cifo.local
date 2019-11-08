c6e4561551610e5368742040d987e629f960e59c
rasepe <52757701+rasepe@users.noreply.github.com>
2019-11-08 18:20:35 +0100

    Update centros.js

--- centros/centros.js
+++ centros/centros.js
@@ -5,6 +5,8 @@ if (window.XMLHttpRequest) {
 	xhr = new ActiveXObject("Microsoft.XMLHTTP");
 }
 xhr.onreadystatechange = function() {
+	// Aviso de carga 
+	document.getElementById("aviso_carga").innerHTML = "un momento por favor";	
 	if (xhr.readyState == 4 && xhr.status == 200) {
 		var arrayCentros = JSON.parse(xhr.responseText);
 		for (var i = 0; i < arrayCentros.records.length; i++) {
@@ -33,10 +35,11 @@ xhr.onreadystatechange = function() {
 				+'</div>'
 				+'</div>'
 				+'</div>';
+			document.getElementById("aviso_carga").innerHTML = "";
 			// Indicamos en que elemento del DOM, vamos a concatenar cada variable "maquetaHTML"
 			document.getElementById("centros").innerHTML += maquetaHTML;
 		}					
 	}
 }
 xhr.open('GET','http://app.cifo.local/api/public/formacion/centros/?nocache='+Math.random(), true);
-xhr.send();	
+xhr.send();
