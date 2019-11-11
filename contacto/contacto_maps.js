b2561db0a5a4fdbafff0b10ee7dc984ccf5c6621
BegonaAramburu <50614087+BegonaAramburu@users.noreply.github.com>
2019-11-11 13:46:06 +0100

    Update contacto_maps.js

--- contacto/contacto_maps.js
+++ contacto/contacto_maps.js
@@ -17,7 +17,6 @@ function initMap(){
 }
 
 
-
 var xhr;
 if (window.XMLHttpRequest) {
 	xhr = new XMLHttpRequest();
@@ -29,72 +28,58 @@ xhr.onreadystatechange = function() {
 	if (xhr.readyState == 4 && xhr.status == 200) {
 		var arrCentros = JSON.parse(xhr.responseText);
 		for (var i = 0; i < arrCentros.records.length; i++) {
-
-			var coordenada = {
-					lat: parseFloat(arrCentros.records[i].lat),
-					lng: parseFloat(arrCentros.records[i].lng),
-			};
-
-
-
-
-			var marker = new google.maps.Marker({
-				position: coordenada,
-				map: map,
-				title: arrCentros.records[i].nombre,
-				label: String.fromCharCode(65 + i)
-			});
-
-
-
-			var infowindow = new google.maps.InfoWindow({
-
-				content: '<div >'
-				+'<div >'
-				//	+'<img src="'+arrCentros.records[i].media+'" class="card-img-top w-100" style="object-fit:cover; max-height:150px" alt="'+arrCentros.records[i].nombre+'">'
-				+'<div >'
-				+'<h5 class="card-title text-info">'+arrCentros.records[i].nombre+'</h5>'
-				+'<div class="border border-light border-right-0 border-left-0 pt-3">'
-				+'<p><i class="fa fa-phone fa-fw text-muted"></i> '+arrCentros.records[i].telefono+'</p>'
-				+'<p><i class="fa fa-envelope fa-fw text-muted"></i>'
-				+'<a href="mailto:'+arrCentros.records[i].buzon+'"> '+arrCentros.records[i].buzon+'</a></p>'
-				+'<p><i class="fa fa-calendar fa-fw text-muted"></i> '+arrCentros.records[i].horario+'</p>'
-				+'<p><i class="fa fa-map-marker fa-fw text-muted"></i> '+arrCentros.records[i].direccion+'</p>'
-				+'</div>'
-				+'<div class="pt-3">'
-				+'<h5 class="text-secondary"><i class="fa fa-road fa-fw text-muted"></i> Acceso</h5>'
-				+arrCentros.records[i].acceso
-				+'</div>'      	
-				+'</div>'
-				+'<div >'
-				+'<p class="d-flex justify-content-center mt-3 align-text-bottom"><a href="#" class="btn btn-primary">Más información...</a></p>'
-				+'</div>'
-				+'</div>'
-				+'</div>'
-				//position: coordenada,
-			});
-			console.log
-			// var positioncentro1 = {lat: 41.41677869999999, lng: 2.1329792000000225};
-			// (arrCentro.records.lat)
-			// (arrCentro.records.lng)
-			/*   google.maps.event.addListener(marker,'click',function() {
-	                	if (!infowindow) { 
-	                		infowindow = new google.maps.InfoWindow();
-	                		}
-	                		infowindow.setContent(marcadores[i][3]);
-	                		infowindow.open(map, marker); });
-			 */
-			/**/
-
-
-
-			google.maps.event.addListener(marker, 'click', function() {	
-				infowindow.open(map, this);
-
-			});
+			crearMarker (arrCentros.records[i],i);
 		}
 	}
 }
 
 xhr.open('GET', 'http://app.cifo.local/api/public/formacion/centros/'+'?nocache='+Math.random(), true);
 xhr.send(null);
+
+//Creamos la función crearMarker que contiene la información de cada marker, trabaja con array de centros
+
+function crearMarker (obj,i) {
+	
+	var coordenada = {
+			lat: parseFloat(obj.lat),
+			lng: parseFloat(obj.lng),
+	};
+
+	var marker = new google.maps.Marker({
+		position: coordenada,
+		map: map,
+		title: obj.nombre,
+		label: String.fromCharCode(65 + i)
+	});
+	
+	var infowindow = new google.maps.InfoWindow({
+
+		content: '<div>'
+		+'<div>'
+		+'<img src="'+obj.media+'" class="card-img-top w-100" style="object-fit:cover; max-height:200px" alt="'+obj.nombre+'">'
+		+'<div >'
+		+'<h5 class="card-title text-info">'+obj.nombre+'</h5>'
+		+'<div class="border border-light border-right-0 border-left-0 pt-3">'
+		+'<p><i class="fa fa-phone fa-fw text-muted"></i> '+obj.telefono+'</p>'
+		+'<p><i class="fa fa-envelope fa-fw text-muted"></i>'
+		+'<a href="mailto:'+obj.buzon+'"> '+obj.buzon+'</a></p>'
+		+'<p><i class="fa fa-calendar fa-fw text-muted"></i> '+obj.horario+'</p>'
+		+'<p><i class="fa fa-map-marker fa-fw text-muted"></i> '+obj.direccion+'</p>'
+		+'</div>'
+		+'<div class="pt-3">'
+		+'<h5 class="text-secondary"><i class="fa fa-road fa-fw text-muted"></i> Acceso</h5>'
+		+obj.acceso
+		+'</div>'      	
+		+'</div>'
+		+'<div >'
+		+'<p class="d-flex justify-content-center mt-3 align-text-bottom"><a href="#" class="btn btn-primary">Más información...</a></p>'
+		+'</div>'
+		+'</div>'
+		+'</div>'	
+	});
+	
+	google.maps.event.addListener(marker, 'click', function() {	
+		infowindow.open(map, this);
+		setTimeout(function () { infowindow.close(); }, 5000);
+			});
+}
